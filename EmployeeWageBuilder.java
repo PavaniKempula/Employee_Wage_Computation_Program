b0f9e2a80e5c72ef11de2a127800324b28c5d20e
PavaniKempula <pavanikempula1998@gmail.com>
2021-07-29 11:05:31 +0530

    MODIFIED : Replaced if else Condition with Switch Blocks

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -2,15 +2,16 @@ package com.bridgelabz;
 
 public class EmployeeWageBuilder {
 
+	private static final int IS_PART_TIME = 1;
+	private static final int IS_FULL_TIME = 2;
+	private static final int WAGE_PER_HR = 20;
+
 	public static void main(String[] args) {
 
 		// Displaying Welcome Message
-		System.out.println("Welcome to Employee Wage Computation Program");
+		System.out.println("Welcome To Employee Wage Computation Program");
 
 		// Variables
-		final int IS_PART_TIME = 1;
-		final int IS_FULL_TIME = 2;
-		final int WAGE_PER_HR = 20;
 		int empHrs = 0;
 		int empWage = 0;
 
@@ -19,15 +20,19 @@ public class EmployeeWageBuilder {
 		 * possible integer number using Math.floor(), By doing % 3 we will only get
 		 * either number 0,1 or 2
 		 */
-		double empCheck = Math.floor(Math.random() * 10) % 3;
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-		// Checking Part Time & Full Time Employee and Wage
-		if (empCheck == IS_PART_TIME) {
+		// Computation using switch case
+		switch (empCheck) {
+		case IS_PART_TIME:
 			empHrs = 4;
-		} else if (empCheck == IS_FULL_TIME) {
+			break;
+		case IS_FULL_TIME:
 			empHrs = 8;
-		} else {
+			break;
+		default:
 			empHrs = 0;
+			break;
 		}
 		empWage = WAGE_PER_HR * empHrs;
 		System.out.println("Employee Wage :" + empWage);
