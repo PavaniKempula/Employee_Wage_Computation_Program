733c4a5513c22084b36b07d022bd9bc23d99457c
PavaniKempula <pavanikempula1998@gmail.com>
2021-07-29 13:09:38 +0530

    MODIFIED : To Calculate Wages Till a Condition of Total Working Hours 100 Or days 20 is Reached for a Month

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,33 +1,37 @@
 package com.bridgelabz;
 
 /**
- * @author Pavani 
- *  With respect to employees attendance,
- *  Calculating employee daily and monthly wage.
+ * @author Pavani
+ * With respect to employees attendance,
+ * Calculating employee daily and monthly wage. 
  */
 public class EmployeeWageBuilder {
-	
+
 	private static final int IS_PART_TIME = 1;
 	private static final int IS_FULL_TIME = 2;
 	private static final int WAGE_PER_HR = 20;
 	private static final int NUM_OF_WORKING_DAYS = 20;
-	
+	private static final int MAX_HRS_IN_MONTH = 100;
+
 	public static void main(String[] args) {
-		
+
 		// Displaying Welcome Message
 		System.out.println("Welcome To Employee Wage Computation Program");
-		
+
 		// Variables
 		int empHrs = 0;
 		int empWage = 0;
 		int totalEmpWage = 0;
-		
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+
 		/*
-		 * Generating random number using math.random(), Converting it to nearest
-		 * possible integer number using Math.floor(), By doing % 3 we will only get
-		 * either number 0,1 or 2
+		 * Generating random number using math.random(),
+		 *  Converting it to nearest possible integer number using Math.floor(),
+		 *   By doing % 3 we will only get either number 0,1 or 2
 		 */
-		for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++) {
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
 			case IS_PART_TIME:
@@ -40,10 +44,13 @@ public class EmployeeWageBuilder {
 				empHrs = 0;
 				break;
 			}
-			empWage = WAGE_PER_HR * empHrs;
-			totalEmpWage += empWage;
-			System.out.println("Employee Wage of Day " + day + " :" + empWage);
+			totalEmpHrs += empHrs;
+			System.out.println("Day :" + totalWorkingDays + "Emp Hrs : " + empHrs);
+
 		}
+		totalEmpWage = totalEmpHrs * WAGE_PER_HR;
+		System.out.println("Total Emp Hrs is:" + totalEmpHrs);
 		System.out.println("Total Emp Wage is :" + totalEmpWage);
 	}
+
 }
