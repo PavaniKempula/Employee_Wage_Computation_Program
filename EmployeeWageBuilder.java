a92d8c6c386b2ab03d345867bbdf0432cff93201
PavaniKempula <pavanikempula1998@gmail.com>
2021-08-09 22:10:26 +0530

    REFACTOR : To Compute

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,10 +1,5 @@
 package com.bridgelabz;
 
-/**
- * @author Pavani
- * With respect to employees attendance,
- * Calculating employee daily and monthly wage. 
- */
 public class EmployeeWageBuilder {
 
 	private static final int IS_PART_TIME = 1;
@@ -13,23 +8,13 @@ public class EmployeeWageBuilder {
 	private static final int NUM_OF_WORKING_DAYS = 20;
 	private static final int MAX_HRS_IN_MONTH = 100;
 
-	public static void main(String[] args) {
-
-		// Displaying Welcome Message
-		System.out.println("Welcome To Employee Wage Computation Program");
-
-		// Variables
+	private static void computeEmpWage(int WAGE_PER_HR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH) {
 		int empHrs = 0;
 		int empWage = 0;
 		int totalEmpWage = 0;
 		int totalEmpHrs = 0;
 		int totalWorkingDays = 0;
 
-		/*
-		 * Generating random number using math.random(),
-		 *  Converting it to nearest possible integer number using Math.floor(),
-		 *   By doing % 3 we will only get either number 0,1 or 2
-		 */
 		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
 			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -51,6 +36,19 @@ public class EmployeeWageBuilder {
 		totalEmpWage = totalEmpHrs * WAGE_PER_HR;
 		System.out.println("Total Emp Hrs is:" + totalEmpHrs);
 		System.out.println("Total Emp Wage is :" + totalEmpWage);
+
+	}
+	
+	/*
+	 * @param args
+	 * Creating objects of a class
+	 * Calling different methods with objects if not static
+	 * Passing values while calling method
+	 */
+	public static void main(String args[]) {
+
+		computeEmpWage(WAGE_PER_HR, NUM_OF_WORKING_DAYS, MAX_HRS_IN_MONTH);
+
 	}
 
 }
