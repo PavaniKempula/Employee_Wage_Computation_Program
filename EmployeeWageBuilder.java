41878d404d9a8a50aa0bfc375e0081b48f06b4bc
PavaniKempula <pavanikempula1998@gmail.com>
2021-08-12 19:47:03 +0530

    ADDED : Used Class method with function parameters for calculating employee wage for multiple companies

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,54 +1,54 @@
 package com.bridgelabz;
 
+import java.lang.*;
+
 public class EmployeeWageBuilder {
 
 	private static final int IS_PART_TIME = 1;
-	private static final int IS_FULL_TIME = 2;
-	private static final int WAGE_PER_HR = 20;
-	private static final int NUM_OF_WORKING_DAYS = 20;
-	private static final int MAX_HRS_IN_MONTH = 100;
+    private static final int IS_FULL_TIME = 2;
+    
+    public static void computEmpWage( ){
 
-	private static void computeEmpWage(int WAGE_PER_HR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH) {
-		int empHrs = 0;
-		int empWage = 0;
-		int totalEmpWage = 0;
-		int totalEmpHrs = 0;
-		int totalWorkingDays = 0;
+        int empHrs = 0;
+        int empWage = 0;
+        int totalEmpWage = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
 
-		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-			totalWorkingDays++;
-			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-			switch (empCheck) {
-			case IS_PART_TIME:
-				empHrs = 4;
-				break;
-			case IS_FULL_TIME:
-				empHrs = 8;
-				break;
-			default:
-				empHrs = 0;
-				break;
-			}
-			totalEmpHrs += empHrs;
-			System.out.println("Day :" + totalWorkingDays + "Emp Hrs : " + empHrs);
+        while(totalEmpHrs<=Company.maxHrsPerMonth && totalWorkingDays<Company.numOfWorkinDays) {
 
-		}
-		totalEmpWage = totalEmpHrs * WAGE_PER_HR;
-		System.out.println("Total Emp Hrs is:" + totalEmpHrs);
-		System.out.println("Total Emp Wage is :" + totalEmpWage);
+            totalWorkingDays++;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            totalEmpHrs += empHrs;
+            System.out.println("Day : " + totalWorkingDays + " Emp Hrs : " + empHrs);
 
-	}
-	
-	/*
-	 * @param args
-	 * Creating objects of a class
-	 * Calling different methods with objects if not static
-	 * Passing values while calling method.
-	 */
-	public static void main(String args[]) {
+        }
 
-		computeEmpWage(WAGE_PER_HR, NUM_OF_WORKING_DAYS, MAX_HRS_IN_MONTH);
+        totalEmpWage =totalEmpHrs *Company.wagePerHrs;
+        System.out.println("Total Emp Hrs is:"+totalEmpHrs);
+        System.out.println("Company is: "+Company.company +", Total Emp Wage is :"+totalEmpWage);
+   // return totalEmpWage;
+}
 
-	}
+    public static void main(String[] args){
+    	EmployeeWageBuilder c = new EmployeeWageBuilder();
+        Company cap = new Company("Capgemini",20,2,10);
+        c.computEmpWage();
+        System.out.println("__________________________________________");
+        Company dmart = new Company("Dmart",20,3,100);
+        c.computEmpWage();
+
+    }
 
 }
