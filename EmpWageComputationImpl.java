e80e040adb6bdb5f12466b2a6598678e89391832
PavaniKempula <pavanikempula1998@gmail.com>
2021-08-13 08:46:05 +0530

    ADDED : Employee Wage of Multiple Companies using interface

--- EmpWageComputationImpl.java
+++ EmpWageComputationImpl.java
@@ -1,57 +1,51 @@
 package com.bridgelabz;
 
-public class EmpWageComputationImpl {
-	private static final int IS_PART_TIME = 1;
-    private static final int IS_FULL_TIME = 2;
-    public int numOfCompany = 0;
-    private Company[] companyAraay;
+import java.util.ArrayList;
 
-    public EmpWageComputationImpl(){
-        companyAraay = new Company[5];
-    }
+public class EmpWageComputationImpl implements EmpWageInterface {
+	public int numOfCompany = 0;
+	private ArrayList<Company> companyAraay;
 
-    void addCompany(String company, int wagePerHrs, int numOfWorkinDays, int maxHrsPerMonth){
-        companyAraay[numOfCompany] = new Company(company, wagePerHrs, numOfWorkinDays, maxHrsPerMonth);
-        numOfCompany++;
-    }
+	public EmpWageComputationImpl() {
+		companyAraay = new ArrayList<Company>();
+	}
 
-    void computEmpWage(){
-        for (int i = 0; i < numOfCompany; i++){
-            companyAraay[i].setTotalEmpWage(this.computEmpWage(companyAraay[i]));
-            System.out.println(companyAraay[i]);
+	public void addCompany(String company, int wagePerHrs, int numOfWorkinDays, int maxHrsPerMonth) {
+		companyAraay.add(numOfCompany, new Company(company, wagePerHrs, numOfWorkinDays, maxHrsPerMonth));
+		numOfCompany++;
+	}
+
+	public void computEmpWage() {
+        for (int i = 0; i < numOfCompany; i++) {
+            companyAraay.get(i).setTotalEmpWage(this.computEmpWage(companyAraay.get(i)));
+            System.out.println(companyAraay.get(i));
         }
+	}
 
-    }
+	public int computEmpWage(Company company) {
+		int empHrs = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
 
-    public static int computEmpWage(Company company ){
+		while (totalEmpHrs <= company.maxHrsPerMonth && totalWorkingDays < company.numOfWorkinDays) {
 
-        int empHrs = 0;
-        int totalEmpHrs = 0;
-        int totalWorkingDays = 0;
+			totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+				break;
+			}
+			totalEmpHrs += empHrs;
+			System.out.println("Day : " + totalWorkingDays + " Emp Hrs : " + empHrs);
+		}
+		return totalEmpHrs * company.wagePerHrs;
+	}
 
-        while(totalEmpHrs <= company.maxHrsPerMonth && totalWorkingDays < company.numOfWorkinDays) {
-
-            totalWorkingDays++;
-            /*
-             * Generating random number using math.random(),
-			 * Converting it to nearest possible integer number using Math.floor(),
-			 * By doing % 3 we will only get either number 0 , 1 or 2. 
-             */
-            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-            switch (empCheck) {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
-            }
-            totalEmpHrs += empHrs;
-            System.out.println("Day : " + totalWorkingDays + " Emp Hrs : " + empHrs);
-        }
-        return totalEmpHrs * company.wagePerHrs;
-    }
 }
--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -8,16 +8,16 @@ package com.bridgelabz;
  * wage as per their Company's parameters
  */
 public class EmployeeWageBuilder {
-	public static void main(String[] args){
-		
-		// Displaying Welcome Message
-        System.out.println("------------------Welcome To Employee Wage Computation------------------");
+	public static void main(String[] args) {
 
-        EmpWageComputationImpl empWageComputation = new EmpWageComputationImpl();
-        empWageComputation.addCompany("Capgemini",20,2,50);
-        System.out.println("--------------------------------------------------------------------");
-        empWageComputation.addCompany("Barclays", 10, 4,100);
-        empWageComputation.computEmpWage();
-        System.out.println("--------------------------------------------------------------------");
-    }
+		// Displaying Welcome Message
+		System.out.println("------------------Welcome To Employee Wage Computation------------------");
+
+		EmpWageComputationImpl empWageComputation = new EmpWageComputationImpl();
+		empWageComputation.addCompany("Capgemini", 20, 2, 50);
+		System.out.println("--------------------------------------------------------------------");
+		empWageComputation.addCompany("Barclays", 10, 4, 100);
+		empWageComputation.computEmpWage();
+	}
+
 }
